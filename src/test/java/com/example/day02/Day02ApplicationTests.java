46469221b2261baf0d32a70f12188005b4579957
up1 <somkiat.p@gmail.com>
2022-02-17 17:36:18 +0700

    Start day 2

---
+++ src/test/java/com/example/day02/Day02ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.day02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+
+@AutoConfigureWireMock()
+class Day02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
