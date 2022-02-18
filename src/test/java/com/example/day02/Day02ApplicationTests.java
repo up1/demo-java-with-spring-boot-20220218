aea078a67b538061af4c91f9f0a3fedaf1c77f34
up1 <somkiat.p@gmail.com>
2022-02-18 14:14:15 +0700

    Let's start

--- src/test/java/com/example/day02/Day02ApplicationTests.java
+++ src/test/java/com/example/day02/Day02ApplicationTests.java
@@ -1,10 +1,7 @@
 package com.example.day02;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 
-@AutoConfigureWireMock()
 class Day02ApplicationTests {
 
 	@Test
