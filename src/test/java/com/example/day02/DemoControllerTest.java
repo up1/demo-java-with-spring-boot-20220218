21942c52f09eae657dd0265c32812ede688022b3
up1 <somkiat.p@gmail.com>
2022-02-18 16:18:34 +0700

    Testing with SpringBootTest

---
+++ src/test/java/com/example/day02/DemoControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.day02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class DemoControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void xxxx() {
+        // Act :: call api
+        DemoResponse result = restTemplate.getForObject("/demo/1", DemoResponse.class);
+        // Assert :: Verify
+        assertEquals(10, result.getCode());
+        assertNull(result.getMessage());
+    }
+
+}
