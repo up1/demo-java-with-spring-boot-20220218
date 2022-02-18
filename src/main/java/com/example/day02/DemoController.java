585b098f498d4822a7b17fa63a00f4a64ba8d5a5
up1 <somkiat.p@gmail.com>
2022-02-18 14:29:05 +0700

    Generate pojo from json

--- src/main/java/com/example/day02/DemoController.java
+++ src/main/java/com/example/day02/DemoController.java
@@ -1,4 +1,15 @@
 package com.example.day02;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+
+    @GetMapping("/demo/{id}")
+    public DemoResponse xxxx(@PathVariable int id) {
+        return null;
+    }
+
 }
---
+++ src/main/java/com/example/day02/DemoResponse.java
@@ -0,0 +1,22 @@
+package com.example.day02;
+
+public class DemoResponse{
+	private int code;
+	private String message;
+
+	public void setCode(int code){
+		this.code = code;
+	}
+
+	public int getCode(){
+		return code;
+	}
+
+	public void setMessage(String message){
+		this.message = message;
+	}
+
+	public String getMessage(){
+		return message;
+	}
+}
